b0e2615f87dc0cb0d6c4647208d0b8697dd7d5f4
elppans <elppansmk@gmail.com>
2024-08-05 09:45:07 -0300

    Update pdv_ip-ping.sh

--- pdv_ip-ping.sh
+++ pdv_ip-ping.sh
@@ -5,7 +5,11 @@ rm -rf ip_OFF.txt >> /dev/null
 
 # shellcheck disable=SC2013
 for IP in $(cat ip.txt); do
-    ping -c 1 "$IP" >>/dev/null &&
-        echo "$IP" >> ip_OK.txt ||
-        echo "$IP OFF" >> ip_OFF.txt
+    if ping -c 1 "$IP" >>/dev/null; then
+        echo "$IP" OK
+        echo "$IP" >>ip_OK.txt
+    else
+        echo "$IP OFF"
+        echo "$IP OFF" >>ip_OFF.txt
+    fi
 done
