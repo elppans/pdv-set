ee75113c8e8ae1c1c4cc9ca93e0c426ee172b961
elppans <elppansmk@gmail.com>
2024-08-05 12:02:26 -0300

    Update file E1

--- pdv_ip-version.sh
+++ pdv_ip-version.sh
@@ -22,8 +22,8 @@ export passwd
 for IP in $(cat ip_OK.txt); do
     echo "$IP" &&
         sshpass -p "$passwd" ssh -o StrictHostKeyChecking=no "$user"@"$IP" "
-        "grep VERSION_ID /etc/os-release" && \
-        grep biblioteca /Zanthus/Zeus/pdvJava/ECFRECEB.CFG && \
+        "grep VERSION_ID /etc/os-release
+        grep biblioteca /Zanthus/Zeus/pdvJava/ECFRECEB.CFG
 	    cat /Zanthus/Zeus/pdvJava/EMUL.INI
 	    echo ""
         "
