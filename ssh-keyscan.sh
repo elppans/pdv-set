8bafe466d02d0241cdc6d7d8ddd9940573bf52cc
elppans <elppansmk@gmail.com>
2024-08-14 11:03:44 -0300

    criado arquivo ssh-keyscan.sh

---
+++ ssh-keyscan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# adicionar um host ao arquivo known_hosts sem precisar acessar o servidor.
+# Comando para escanear a chave pública do host e adicioná-la ao arquivo known_hosts
+
+#ssh-keyscan -H 10.100.135.83 >> ~/.ssh/known_hosts
+
+#!/bin/bash
+
+IP="$1"
+KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
+
+# Verifica se o IP já está no arquivo known_hosts
+if ssh-keygen -F "$IP" > /dev/null; then
+    echo "O IP $IP já está no arquivo known_hosts."
+else
+    echo "Adicionando o IP $IP ao arquivo known_hosts."
+    ssh-keyscan -H "$IP" >> "$KNOWN_HOSTS_FILE"
+    echo "IP $IP adicionado com sucesso."
+fi
