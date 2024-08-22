a08c17135df31c8347e67bb1b3cdb897721f5237
elppans <elppansmk@gmail.com>
2024-08-22 16:40:39 -0300

    Adicionado pdv-set*FLAGS.sh

---
+++ pdv-set_FW_FLAGS-1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Verifica se o primeiro parâmetro foi fornecido
+if [ -z "$1" ]; then
+    echo "Você não forneceu nenhum valor. Por favor, insira um valor para o usuario."
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Você não forneceu nenhum valor. Por favor, insira um valor para a senha."
+    exit 1
+fi
+
+# Se o parâmetro foi fornecido, atribui-o à variável 'user'
+user="$1"
+passwd="$2"
+export user
+export passwd
+
+# Executar comandos via SSH, usando IP atribuido ao arquivo ip_OK.txt
+# shellcheck disable=SC2013
+for IP in $(cat ip_OK.txt); do
+    ./ssh-keyscan.sh "$IP" &>>/dev/null
+    echo "$IP"
+    sshpass -p "$passwd" ssh -o StrictHostKeyChecking=no "$user"@"$IP" \
+        "
+echo ""$passwd"" | sudo -S sed -i '/FW_FLAGS/d' /Zanthus/Zeus/pdvJava/EMUL.INI
+echo ""$passwd"" | echo -e 'FW_FLAGS=1' | sudo -S tee -a /Zanthus/Zeus/pdvJava/EMUL.INI &>>/dev/null
+echo ""
+grep biblioteca /Zanthus/Zeus/pdvJava/ECFRECEB.CFG
+cat /Zanthus/Zeus/pdvJava/EMUL.INI
+echo "..."
+"
+done
