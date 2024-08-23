a1455b60d93bbdb23301b45a11181d338dee5e44
elppans <elppansmk@gmail.com>
2024-08-23 15:25:33 -0300

    criado/modificado .sh

--- pdv-set_flagsmod.sh
+++ pdv-set_flagsmod.sh
@@ -26,13 +26,13 @@ for IP in $(cat ip_OK.txt); do
     sshpass -p "$passwd" ssh -o StrictHostKeyChecking=no "$user"@"$IP" \
         "
         # cmd
-        # source /Zanthus/Zeus/pdvJava/EMUL.INI  &>>/dev/null
-        # echo ""$passwd"" | sudo -S sed -i "s/FW_FLAGS="${FW_FLAGS}"/FW_FLAGS=2/" /Zanthus/Zeus/pdvJava/EMUL.INI
-        # echo ""$passwd"" | sudo -S sed -i "s/FW_MODELO_IMPRESSORA="${FW_MODELO_IMPRESSORA}"/FW_MODELO_IMPRESSORA=0/" /Zanthus/Zeus/pdvJava/EMUL.INI
+        source /Zanthus/Zeus/pdvJava/EMUL.INI  &>>/dev/null
+        echo ""$passwd"" | sudo -S sed -i "s/FW_FLAGS="${FW_FLAGS}"/FW_FLAGS=2/" /Zanthus/Zeus/pdvJava/EMUL.INI
+        echo ""$passwd"" | sudo -S sed -i "s/FW_MODELO_IMPRESSORA="${FW_MODELO_IMPRESSORA}"/FW_MODELO_IMPRESSORA=0/" /Zanthus/Zeus/pdvJava/EMUL.INI
         echo ""$passwd"" | sudo -S rm -rf /opt/pdv_e1update &>>/dev/null
         echo ""
-        # cat /Zanthus/Zeus/pdvJava/ECFRECEB.CFG
-        # cat /Zanthus/Zeus/pdvJava/EMUL.INI
+        cat /Zanthus/Zeus/pdvJava/ECFRECEB.CFG
+        cat /Zanthus/Zeus/pdvJava/EMUL.INI
         echo "..."
         # cmd
         "
