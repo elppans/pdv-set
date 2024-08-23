f85604ca1e25e26982d5b1ff16b7608602a02eb0
elppans <elppansmk@gmail.com>
2024-08-23 15:56:26 -0300

    criado/modificado .sh

--- pdv-set_ping.sh
+++ pdv-set_ping.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-IP_FILE="ip.txt"
+IP_DIR="$HOME/.ip"
+IP_FILE="$IP_DIR/ip.txt"
+IP_OK_FILE="$IP_DIR/ip_ok.txt"
+IP_OFF_FILE="$IP_DIR/ip_off.txt"
+
+mkdir -p "$IP_DIR"
 
 # Função para verificar se o arquivo ip.txt existe
 check_ip_file_exists() {
@@ -13,16 +18,16 @@ check_ip_file_exists() {
 # Executa função de verificação do arquivo ip.txt
 check_ip_file_exists
 
-rm -rf ip_OK.txt >> /dev/null
-rm -rf ip_OFF.txt >> /dev/null
+rm -rf "$IP_OK_FILE" >> /dev/null
+rm -rf "$IP_OFF_FILE" >> /dev/null
 
 # shellcheck disable=SC2013
-for IP in $(cat ip.txt); do
+for IP in $(cat "$IP_FILE"); do
     if ping -c 1 "$IP" >>/dev/null; then
         echo "$IP" OK
-        echo "$IP" >>ip_OK.txt
+        echo "$IP" >>"$IP_OK_FILE"
     else
         echo "$IP OFF"
-        echo "$IP" >>ip_OFF.txt
+        echo "$IP" >>"$IP_OFF_FILE"
     fi
 done
