5d4a9a2858da26e115d7d2d97718e1794db33811
elppans <elppansmk@gmail.com>
2024-08-26 09:21:17 -0300

    Desativado linha 37, FW_FLAGS

--- pdv-set_flagsmod.sh
+++ pdv-set_flagsmod.sh
@@ -34,13 +34,14 @@ for IP in $(cat "$IP_OK_FILE"); do
         "
         # cmd
         source /Zanthus/Zeus/pdvJava/EMUL.INI  &>>/dev/null
-        echo ""$passwd"" | sudo -S sed -i "s/FW_FLAGS="${FW_FLAGS}"/FW_FLAGS=2/" /Zanthus/Zeus/pdvJava/EMUL.INI
+        #echo ""$passwd"" | sudo -S sed -i "s/FW_FLAGS="${FW_FLAGS}"/FW_FLAGS=2/" /Zanthus/Zeus/pdvJava/EMUL.INI
         echo ""$passwd"" | sudo -S sed -i "s/FW_MODELO_IMPRESSORA="${FW_MODELO_IMPRESSORA}"/FW_MODELO_IMPRESSORA=0/" /Zanthus/Zeus/pdvJava/EMUL.INI
         echo ""$passwd"" | sudo -S rm -rf /opt/pdv_e1update &>>/dev/null
         echo ""
-        cat /Zanthus/Zeus/pdvJava/ECFRECEB.CFG
+        #cat /Zanthus/Zeus/pdvJava/ECFRECEB.CFG
         cat /Zanthus/Zeus/pdvJava/EMUL.INI
         echo "..."
         # cmd
         "
 done
+
